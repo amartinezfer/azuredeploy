b2dff45471db7c011fac5b7ef875afa7c391778a
amartinezfer <amartinezfer@gmail.com>
2023-02-25 21:28:24 +0100

    add inventory to ansible

---
+++ terraform/inventory.tf
@@ -0,0 +1,13 @@
+
+
+ locals {
+    inventory_config = templatefile("${path.module}/templates/ansible_inventory.tftpl", {
+       vm = azurerm_public_ip.unirpublicip.ip_address,
+      kube = azurerm_public_ip.kubeunirpublicip.ip_address
+    })
+}
+
+resource "local_file" "inventory" {
+    content  = local.inventory_config
+    filename = "${path.module}/../ansible/inventory.ini"
+}
