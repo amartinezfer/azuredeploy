fe29fc7cfce0b1f6ec2c309c390560eac5ca822d
amartinezfer <amartinezfer@gmail.com>
2023-03-04 07:57:27 +0100

    modify inventory

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -1,14 +1,17 @@
 
+data "azurerm_subscription" "current" {}
 
  locals {
     inventory_config = templatefile("${path.module}/templates/ansible_inventory.tftpl", {
       vm = azurerm_public_ip.unirpublicip.ip_address,
-      kube = azurerm_public_ip.kubeunirpublicip.ip_address
       idrsa = "${var.idRsaPathPrivate}"
       user = "${var.adminUser}"
       register = azurerm_container_registry.uniracr.login_server
       register_user = azurerm_container_registry.uniracr.admin_username
       register_pass = azurerm_container_registry.uniracr.admin_password
+      rsg = azurerm_resource_group.unir.name
+      sub = data.azurerm_subscription.current.subscription_id
+      ipmongo = azurerm_public_ip.mongo.ip_address
     })
 }
 
