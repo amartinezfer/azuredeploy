50d27194ce9550e1f1ef9351f6a3f87a936ab444
amartinezfer <amartinezfer@gmail.com>
2023-03-04 07:57:37 +0100

    modify inventory

--- terraform/kube.tf
+++ terraform/kube.tf
@@ -1,30 +1,4 @@
 
-# Crea una dirección IP pública kube
-resource "azurerm_public_ip" "kubeunirpublicip" {
-  name                = "public-ip-kube"
-  location            = azurerm_resource_group.unir.location
-  resource_group_name = azurerm_resource_group.unir.name
-  allocation_method   = "Static"
-  sku = "Standard"
-}
-
-
-
-# apunta la ip publica a url
-resource "azurerm_dns_a_record" "dns_a_record_kube" {
-  name                = "kube.unir.io"
-  zone_name           = azurerm_dns_zone.unirdnszone.name
-  resource_group_name = azurerm_resource_group.unir.name
-  ttl                 = 300
-
-  records = [
-    azurerm_public_ip.kubeunirpublicip.ip_address
-  ]
-}
-
-
-
-
 resource "azurerm_kubernetes_cluster" "unirkube" {
   name                = "unir-kubernetes-cluster"
   location            = azurerm_resource_group.unir.location
@@ -56,21 +30,25 @@ resource "azurerm_kubernetes_cluster" "unirkube" {
     network_plugin = "azure"
     load_balancer_sku = "standard"
 
-    load_balancer_profile {
-      outbound_ip_address_ids = [azurerm_public_ip.kubeunirpublicip.id]
-    }
+    #load_balancer_profile {
+    #  outbound_ip_address_ids = [azurerm_public_ip.kubeunirpublicip.id]
+    #}
   }
 
 }
 
-#resource "azurerm_role_assignment" "aks_cluster" {
-#  scope                = azurerm_kubernetes_cluster.unirkube.id
-#  role_definition_name = "Contributor"
-#  principal_id         =azuread_application.unirapp.application_id
-#  depends_on = [
-#    azurerm_kubernetes_cluster.unirkube
-#  ]
-#}
+
+resource "local_file" "kubeconfig" {
+  filename = "../ansible/kube.config"
+  content  = azurerm_kubernetes_cluster.unirkube.kube_config_raw
+}
 
 
+resource "azurerm_public_ip" "mongo" {
+  name                = "mongoip"
+  location            = azurerm_resource_group.unir.location
+  resource_group_name = azurerm_resource_group.unir.name
 
+  allocation_method = "Static"
+  sku               = "Standard"
+}
